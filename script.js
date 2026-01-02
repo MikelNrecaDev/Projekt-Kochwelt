f125bbcf2b9137f79dfecc338a82fb8ec4438cb9
MikelNrecaDev <mikelnreca@hotmail.com>
2026-01-02 22:31:01 +0100

    fertig

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //function for contact.html
 // sendMail()
-function sendMail(event){﻿
+function sendMail(event) {
     event.preventDefault();
     const data = new FormData(event.target);
 
@@ -17,7 +17,7 @@ function sendMail(event){﻿
     });
 }
 
-document.querySelector('form').addEventListener('submit', sendMail);
+
 
 
 ///Rezept Seite
@@ -43,29 +43,63 @@ const basisMenge = {
 
 //Schritt 1: Funktionsname frei definieren zum Festlegen des Funktionsinhaltes
 function berechnePortionen() {
-console.log
+    console.log
 
-//Schritt 2: Anzahl der Portionen aus dem Input-Feld holen
-const portionen = parseFloat(document.getElementById('portionenInput').value) || 1;
+    //Schritt 2: Anzahl der Portionen aus dem Input-Feld holen
+    const portionen = parseFloat(document.getElementById('portionenInput').value) || 1;
 
-///Rechnung erstellen
-document.getElementById('eigelb').textContent = (basisMenge.eigelb * portionen);
-document.getElementById('senf').textContent = (basisMenge.senf * portionen);
-document.getElementById('zitronensaft').textContent = (basisMenge.zitronensaft * portionen).toFixed(1);
-document.getElementById('worcestershire').textContent = (basisMenge.worcestershire * portionen);
-document.getElementById('sonnenblumenoel').textContent = (basisMenge.sonnenblumenoel * portionen);
-document.getElementById('salzoben').textContent = (basisMenge.salzoben * portionen);
-document.getElementById('pfeffer').textContent = (basisMenge.pfeffer * portionen);
-document.getElementById('toast').textContent = (basisMenge.toast * portionen);
-document.getElementById('speck').textContent = (basisMenge.speck * portionen);
-document.getElementById('haenchen').textContent = (basisMenge.haenchen * portionen);
-document.getElementById('salzunten').textContent = (basisMenge.salzunten * portionen).toFixed(1);
-document.getElementById('lattich').textContent = (basisMenge.lattich * portionen);
-document.getElementById('gurke').textContent = (basisMenge.gurke * portionen);
-document.getElementById('sbrinz').textContent = (basisMenge.sbrinz * portionen);
+    ///Rechnung erstellen
+    document.getElementById('eigelb').textContent = (basisMenge.eigelb * portionen);
+    document.getElementById('senf').textContent = (basisMenge.senf * portionen);
+    document.getElementById('zitronensaft').textContent = (basisMenge.zitronensaft * portionen).toFixed(1);
+    document.getElementById('worcestershire').textContent = (basisMenge.worcestershire * portionen);
+    document.getElementById('sonnenblumenoel').textContent = (basisMenge.sonnenblumenoel * portionen);
+    document.getElementById('salzoben').textContent = (basisMenge.salzoben * portionen);
+    document.getElementById('pfeffer').textContent = (basisMenge.pfeffer * portionen);
+    document.getElementById('toast').textContent = (basisMenge.toast * portionen);
+    document.getElementById('speck').textContent = (basisMenge.speck * portionen);
+    document.getElementById('haenchen').textContent = (basisMenge.haenchen * portionen);
+    document.getElementById('salzunten').textContent = (basisMenge.salzunten * portionen).toFixed(1);
+    document.getElementById('lattich').textContent = (basisMenge.lattich * portionen);
+    document.getElementById('gurke').textContent = (basisMenge.gurke * portionen);
+    document.getElementById('sbrinz').textContent = (basisMenge.sbrinz * portionen);
 
 }
+
+//Legt die Grenzen fest zwischen einer Portion bis 20 Portionen
+function checkValue() {
+
+    let myvalue = document.getElementById("portionenInput").value;
+
+    if (myvalue < 1 || myvalue > 20) {
+        document.getElementById('eigelb').textContent = "0.25";
+        document.getElementById('senf').textContent = "0.25";
+        document.getElementById('zitronensaft').textContent = "0.63";
+        document.getElementById('worcestershire').textContent = "0.25";
+        document.getElementById('sonnenblumenoel').textContent = "1.25";
+        document.getElementById('salzoben').textContent = "0.13";
+        document.getElementById('pfeffer').textContent = "1";
+        document.getElementById('toast').textContent = "1";
+        document.getElementById('speck').textContent = "3";
+        document.getElementById('haenchen').textContent = "100";
+        document.getElementById('salzunten').textContent = "0.5"
+        document.getElementById('lattich').textContent = "0.5";
+        document.getElementById('gurke').textContent = "50";
+        document.getElementById('sbrinz').textContent = "53";
+    
+
+        console.log("Wert ist nicht ok")
+
+    }
+    else {
+
+        console.log("Wert ist ok")
+
+    }
+}
 //Schritt 3: Event Listener für den Button  --> Button reagiert auf den klick und rechnet die funktion *berechnePortionen* aus (Schritt 1)
-                        //portionenButton muss als Id innerhalb des button tags stehen// 
+//portionenButton muss als Id innerhalb des button tags stehen// 
 document.getElementById('portionenButton').addEventListener('click', berechnePortionen);
+document.getElementById('portionenButton').addEventListener('click', checkValue);
+
 
